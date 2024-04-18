29917cfbaa6abca6c3cad6f2fa5e27ec3fbc1f56
abderrazzakSakoum <a.sakoum.dev@gmail.com>
2024-04-18 23:06:46 +0100

    okey pour le site

--- src/app/pages/it-solutions/page-service-detail/page-service-detail.component.ts
+++ src/app/pages/it-solutions/page-service-detail/page-service-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-service-detail',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-service-detail.component.css']
 })
 export class PageServiceDetailComponent {
+  @Input() from = 1;
 
 }
--- src/app/sections/home/home2/section-about-company3/section-about-company3.component.ts
+++ src/app/sections/home/home2/section-about-company3/section-about-company3.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-section-about-company3',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./section-about-company3.component.css']
 })
 export class SectionAboutCompany3Component {
-
+  from: number = 1;
+  constructor(
+    private route: ActivatedRoute
+  ){
+    this.route.fragment.subscribe(fragment => {
+      if(fragment)
+        this.from = +fragment;
+      // Do whatever you want with the fragment value
+    });
+  }
 }
